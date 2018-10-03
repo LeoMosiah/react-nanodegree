5805fbb248555ad2998db6ad2eadd775f55ad6b1
LeoMosiah <leonardomosiah@gmail.com>
2018-10-03 07:15:43 -0300

    adding contacts

---
+++ Modulo Um/contacts/src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import ListContacts from "./ListContacts";
+import * as ContactsAPI from "./utils/ContactsAPI";
+import CreateContact from "./CreateContact";
+
+class App extends Component {
+  state = {
+    contacts: [],
+    screen: "list"
+  };
+  componentDidMount() {
+    ContactsAPI.getAll().then(contacts => {
+      this.setState(() => ({ contacts }));
+    });
+  }
+  removeContact = contact => {
+    this.setState(currentState => ({
+      contacts: currentState.contacts.filter(current => {
+        return current.id !== contact.id;
+      })
+    }));
+    ContactsAPI.remove(contact);
+  };
+  render() {
+    return (
+      <div>
+        {this.state.screen === "list" && (
+          <ListContacts
+            contacts={this.state.contacts}
+            onDeleteContact={this.removeContact}
+            onNavigate={() => {
+              this.setState(() => ({
+                scren: "create"
+              }));
+            }}
+          />
+        )}
+        {this.state.screen === "create" && <CreateContact />}
+      </div>
+    );
+  }
+}
+
+export default App;
