9168557a5815a563ae861450c7a94b86426c7e78
LeoMosiah <37636805+LeoMosiah@users.noreply.github.com>
2018-10-05 14:17:45 -0300

    Update BookShelf.js

--- Modulo Um/myreads/src/wrappers/BookShelf.js
+++ Modulo Um/myreads/src/wrappers/BookShelf.js
@@ -1,16 +1,18 @@
 import React, { Component } from "react";
-import { Book } from "../components/Book";
+import Book from "../components/Book";
 
 class BookShelf extends Component {
   render() {
+    const { title } = this.props;
     return (
       <div className="bookshelf">
-        <h2 className="bookshelf-title">Currently Reading</h2>
+        <h2 className="bookshelf-title">{title}</h2>
         <div className="bookshelf-books">
-          <ol className="books-grid" />
-          <li>
-            <Book />
-          </li>
+          <ol className="books-grid">
+            <li>
+              <Book title="To Kill a Mockingbird" author="Harper Lee" />
+            </li>
+          </ol>
         </div>
       </div>
     );
