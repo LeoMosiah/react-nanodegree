ce55c3cd8ce01814b1cfa7b2c717dadec044739c
LeoMosiah <37636805+LeoMosiah@users.noreply.github.com>
2018-10-08 14:57:36 -0300

    Update SearchBooks.js

--- Modulo Um/myreads/src/components/SearchBooks.js
+++ Modulo Um/myreads/src/components/SearchBooks.js
@@ -1,45 +1,56 @@
 import React, { Component } from "react";
-import {Link} from "react-router-dom";
-import * as BooksAPI from "../utils/BooksAPI"
+import { Link } from "react-router-dom";
+import * as BooksAPI from "../utils/BooksAPI";
+import Book from "./Book";
 
 class SearchBooks extends Component {
-    state = {
-        input : "",
-        books : []
-    }
-    updateQuery = (input) =>{
-        this.setState(() =>({
-            input:input.trim()
-        }))
-    }
-    search = book => {
-        BooksAPI.search(book).then(book => {
-            this.setState(currentState => ({
-                books: currentState.books.concat([book])
-            }));
-        });
-    };
+  state = {
+    input: "",
+    books: [],
+    shelf: "Search"
+  };
+  updateQuery = input => {
+    this.setState(() => ({
+      input: input.trim()
+    }));
+  };
+  searchBook = query => {
+    BooksAPI.search(query).then(books => {
+      books.map(book => this.state.books.push(book));
+    });
+  };
   render() {
-      return <div className="search-books">
-          <div className="search-books-bar">
-              <Link
-                  className="close-search"
-                  to="/"
-              >
-                  Close
-              </Link>
-              <div className="search-books-input-wrapper">
-                  <input type="text" placeholder="Search by title or author"
-                         value={this.state.input}
-                         onChange={(event) =>(this.updateQuery(event.target.value))}
-                  />
-              </div>
+    const { input, books } = this.state;
+    return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link className="close-search" to="/">
+            Close
+          </Link>
+          <div className="search-books-input-wrapper">
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={input}
+              onChange={event => this.updateQuery(event.target.value)}
+            />
           </div>
-          <div className="search-books-results">
-              <ol className="books-grid"/>
-              <li>{console.log(BooksAPI.search("Art").then(res => res))}</li>
-          </div>
-      </div>;
+        </div>
+        <div className="search-books-results">
+          {this.searchBook("Art")}
+          <ol className="books-grid">
+            {books.map(book => (
+              <Book
+                key={book.id}
+                title={`${book.title}`}
+                author={book.authors.join(" ")}
+                coverURL={book.imageLinks.smallThumbnail}
+              />
+            ))}
+          </ol>
+        </div>
+      </div>
+    );
   }
 }
 
