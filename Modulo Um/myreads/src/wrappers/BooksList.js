ec9d29df1dd2d3e5bb5c64c891569175b37f4259
LeoMosiah <37636805+LeoMosiah@users.noreply.github.com>
2018-10-10 12:10:51 -0300

    Update BooksList.js

--- Modulo Um/myreads/src/wrappers/BooksList.js
+++ Modulo Um/myreads/src/wrappers/BooksList.js
@@ -4,15 +4,16 @@ import OpenSearch from "../atoms/OpenSearch";
 
 class BooksList extends Component {
   render() {
+    const { books } = this.props;
     return (
       <div className="list-books">
         <div className="list-books-title">
           <h1>MyReads</h1>
         </div>
         <div className="list-books-content">
-          <BookShelf title="Currently Reading" />
-          <BookShelf title="Want To Read" />
-          <BookShelf title="Read" />
+          <BookShelf title="Currently Reading" books={books} />
+          <BookShelf title="Want To Read" books={books} />
+          <BookShelf title="Read" books={books} />
         </div>
         <OpenSearch />
       </div>
