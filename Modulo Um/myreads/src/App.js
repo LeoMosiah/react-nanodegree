3c6f977590765ca389f48f8998a33de154cd00e0
LeoMosiah <37636805+LeoMosiah@users.noreply.github.com>
2018-10-10 12:11:18 -0300

    Update App.js

--- Modulo Um/myreads/src/App.js
+++ Modulo Um/myreads/src/App.js
@@ -1,25 +1,29 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import "./App.css";
 import BooksList from "./wrappers/BooksList";
-import {Route} from "react-router-dom";
+import { Route } from "react-router-dom";
 import SearchBooks from "./components/SearchBooks";
-import * as BooksAPI from "./utils/BooksAPI"
+import * as BooksAPI from "./utils/BooksAPI";
 
 class BooksApp extends React.Component {
-    state = {
-        books: []
-    };
-    componentDidMount() {
-        BooksAPI.getAll().then(books => {
-            this.setState(() => ({ books }));
-        });
-    }
+  state = {
+    books: []
+  };
+  componentDidMount() {
+    BooksAPI.getAll().then(books => {
+      this.setState(() => ({ books }));
+    });
+  }
   render() {
     return (
-        <div className="app">
-            < Route exact path="/" component={BooksList}/>
-            <Route path="/search" component={SearchBooks}/>
-        </div>
+      <div className="app">
+        <Route
+          exact
+          path="/"
+          render={() => <BooksList books={this.state.books} />}
+        />
+        <Route path="/search" component={SearchBooks} />
+      </div>
     );
   }
 }
