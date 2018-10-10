42a126b8aad0d4d23eab21fdf8892026361185aa
LeoMosiah <37636805+LeoMosiah@users.noreply.github.com>
2018-10-10 13:44:20 -0300

    Update BookShelfChanger.js

--- Modulo Um/myreads/src/atoms/BookShelfChanger.js
+++ Modulo Um/myreads/src/atoms/BookShelfChanger.js
@@ -1,19 +1,30 @@
 import React, { Component } from "react";
+import * as BooksAPI from "../utils/BooksAPI";
 
-const BookShelfChanger = props => {
-  return (
-    <div className="book-shelf-changer">
-      <select>
-        <option value="move" disabled>
-          Move to...
-        </option>
-        <option value="currentlyReading">Currently Reading</option>
-        <option value="wantToRead">Want to Read</option>
-        <option value="read">Read</option>
-        <option value="none">None</option>
-      </select>
-    </div>
-  );
-};
-
+class BookShelfChanger extends Component {
+  state = {
+    shelf: []
+  };
+  handleChange = (bookId, shelf) =>
+    shelf !== "none" ? BooksAPI.update({ id: bookId }, shelf) : null;
+  render() {
+    const { bookID } = this.props;
+    return (
+      <div className="book-shelf-changer">
+        <select
+          defaultValue="move"
+          onChange={event => this.handleChange(bookID, event.target.value)}
+        >
+          <option value="move" disabled>
+            Move to...
+          </option>
+          <option value="currentlyReading">Currently Reading</option>
+          <option value="wantToRead">Want to Read</option>
+          <option value="read">Read</option>
+          <option value="none">None</option>
+        </select>
+      </div>
+    );
+  }
+}
 export default BookShelfChanger;
