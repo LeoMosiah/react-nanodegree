579e2e338852f91d12477dd065fba29f74914088
LeoMosiah <37636805+LeoMosiah@users.noreply.github.com>
2018-10-10 12:09:51 -0300

    Update BookShelf.js

--- Modulo Um/myreads/src/wrappers/BookShelf.js
+++ Modulo Um/myreads/src/wrappers/BookShelf.js
@@ -2,13 +2,30 @@ import React, { Component } from "react";
 import Book from "../components/Book";
 
 class BookShelf extends Component {
+  byShelf = book => {};
+  camelize = str => {
+    return str.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function(match, index) {
+      if (+match === 0) return ""; // or if (/\s+/.test(match)) for white spaces
+      return index == 0 ? match.toLowerCase() : match.toUpperCase();
+    });
+  };
   render() {
-    const { title } = this.props;
+    const { title, books } = this.props;
     return (
       <div className="bookshelf">
         <h2 className="bookshelf-title">{title}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
+            {books
+              .filter(book => book.shelf === this.camelize(title))
+              .map(book => (
+                <Book
+                  key={book.id}
+                  title={`${book.title}`}
+                  author={book.authors}
+                  coverURL={book.imageLinks.smallThumbnail}
+                />
+              ))}
           </ol>
         </div>
       </div>
