1dd98eac3ca0169521c24f50385aef8ea5452802
LeoMosiah <37636805+LeoMosiah@users.noreply.github.com>
2018-10-17 09:55:32 -0300

    Update BooksList.js

--- Modulo Um/myreads/src/pages/BooksList.js
+++ Modulo Um/myreads/src/pages/BooksList.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import BookShelf from "../components/BookShelf";
-import OpenSearch from "../components/OpenSearch";
+import { Link } from "react-router-dom";
 import * as BooksAPI from "../utils/BooksAPI";
 
 class BooksList extends Component {
@@ -12,6 +12,22 @@ class BooksList extends Component {
       books: await BooksAPI.getAll()
     });
   }
+  handleChange = (bookId, shelf) => {
+    if (shelf !== "none") {
+      BooksAPI.update({ id: bookId }, shelf).then(() => {
+        this.setState(prevState => ({
+          books: prevState.books.map(book => {
+            if (book.id === bookId) {
+              book.shelf = shelf;
+              return book;
+            } else {
+              return book;
+            }
+          })
+        }));
+      });
+    }
+  };
   render() {
     const { books } = this.state;
     return (
@@ -20,14 +36,34 @@ class BooksList extends Component {
           <h1>MyReads</h1>
         </div>
         <div className="list-books-content">
-          <BookShelf title="Currently Reading" books={books} />
-          <BookShelf title="Want To Read" books={books} />
-          <BookShelf title="Read" books={books} />
+          <BookShelf
+            title="Currently Reading"
+            books={books}
+            handleChange={this.handleChange}
+          />
+          <BookShelf
+            title="Want To Read"
+            books={books}
+            handleChange={this.handleChange}
+          />
+          <BookShelf
+            title="Read"
+            books={books}
+            handleChange={this.handleChange}
+          />
         </div>
         <OpenSearch />
       </div>
     );
   }
 }
+
+const OpenSearch = () => {
+  return (
+    <div className="open-search">
+      <Link to="/search">Add a book</Link>
+    </div>
+  );
+};
 
 export default BooksList;
