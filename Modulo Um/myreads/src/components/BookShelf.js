1c19ac0e1b554a592c6b2f4744b592529b8da7f6
LeoMosiah <37636805+LeoMosiah@users.noreply.github.com>
2018-10-19 13:07:45 -0300

    Update BookShelf.js

--- Modulo Um/myreads/src/components/BookShelf.js
+++ Modulo Um/myreads/src/components/BookShelf.js
@@ -4,27 +4,21 @@ import _ from "lodash";
 
 class BookShelf extends Component {
   render() {
-    const { title, books, handleChange } = this.props;
-      return <div className="bookshelf">
-          <h2 className="bookshelf-title">{title}</h2>
-          <div className="bookshelf-books">
-              <ol className="books-grid">
-                  {books
-                      .filter(book => book.shelf === _.camelCase(title))
-                      .map(book => (
-                          <Book
-                              key={book.id}
-                              id={book.id}
-                              title={book.title}
-                              author={book.authors}
-                              coverURL={book.imageLinks.thumbnail}
-                              handleChange={handleChange}
-                              shelf={book.shelf}
-                          />
-                      ))}
-              </ol>
-          </div>
-      </div>;
+    const { shelfTitle, shelfBooks, handleChange } = this.props;
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{shelfTitle}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {shelfBooks
+              .filter(book => book.shelf === _.camelCase(shelfTitle))
+              .map(book => (
+                <Book key={book.id} book={book} handleChange={handleChange} />
+              ))}
+          </ol>
+        </div>
+      </div>
+    );
   }
 }
 
