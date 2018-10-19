5ed2e22900f633a2739514a2ab426f55a7633f07
LeoMosiah <37636805+LeoMosiah@users.noreply.github.com>
2018-10-19 13:07:09 -0300

    Update Book.js

--- Modulo Um/myreads/src/components/Book.js
+++ Modulo Um/myreads/src/components/Book.js
@@ -3,60 +3,56 @@ import { SelectMenu, Button } from "evergreen-ui";
 import _ from "lodash";
 
 class Book extends Component {
-    render() {
-        const { title, author, coverURL, id, handleChange, shelf } = this.props;
-        return (
-            <div className="book">
-                <div className="book-top">
-                    <div
-                        className="book-cover"
-                        style={{
-                            width: 128,
-                            height: 193,
-                            backgroundImage: `url(${coverURL})`
-                        }}
-                    />
-                    <BookShelfChanger
-                        bookID={id}
-                        handleChange={handleChange}
-                        shelf={shelf}
-                    />
-                </div>
-                <div className="book-title">{title}</div>
-                <div className="book-authors">{author}</div>
-            </div>
-        );
-    }
+  render() {
+    const { book, handleChange } = this.props;
+    return (
+      <div className="book">
+        <div className="book-top">
+          <div
+            className="book-cover"
+            style={{
+              width: 128,
+              height: 193,
+              backgroundImage: `url(${book.imageLinks.thumbnail})`
+            }}
+          />
+          <BookShelfChanger handleChange={handleChange} book={book} />
+        </div>
+        <div className="book-title">{book.title}</div>
+        <div className="book-authors">{book.authors}</div>
+      </div>
+    );
+  }
 }
 
 class BookShelfChanger extends Component {
-    render() {
-        const { bookID, handleChange, shelf } = this.props;
-        const bookHasShelf = shelf =>
-            shelf !== undefined ? _.startCase(shelf) : "None";
-        return (
-            <div className="book-shelf-changer">
-                <SelectMenu
-                    hasFilter={false}
-                    hasTitle={false}
-                    height={170}
-                    width={150}
-                    title="Select name"
-                    options={[
-                        "Move to ...",
-                        "Currently Reading",
-                        "Want To Read",
-                        "Read",
-                        "None"
-                    ].map(label => ({ label, value: label }))}
-                    selected={bookHasShelf(shelf)}
-                    onSelect={item => handleChange(bookID, _.camelCase(item.value))}
-                >
-                    <Button className="hidden" />
-                </SelectMenu>
-            </div>
-        );
-    }
+  render() {
+    const { book, handleChange } = this.props;
+    const bookHasShelf = shelf =>
+      shelf !== undefined ? _.startCase(shelf) : "None";
+    return (
+      <div className="book-shelf-changer">
+        <SelectMenu
+          hasFilter={false}
+          hasTitle={false}
+          height={170}
+          width={150}
+          title="Select name"
+          options={[
+            "Move to ...",
+            "Currently Reading",
+            "Want To Read",
+            "Read",
+            "None"
+          ].map(label => ({ label, value: label }))}
+          selected={bookHasShelf(book.shelf)}
+          onSelect={item => handleChange(book, _.camelCase(item.value))}
+        >
+          <Button className="hidden" />
+        </SelectMenu>
+      </div>
+    );
+  }
 }
 
 export default Book;
