4d5682fb813a4a41f15b21ae43e598c2f4cebe4a
LeoMosiah <37636805+LeoMosiah@users.noreply.github.com>
2018-10-19 13:04:55 -0300

    Update App.js

--- Modulo Um/myreads/src/App.js
+++ Modulo Um/myreads/src/App.js
@@ -4,33 +4,42 @@ import { Route } from "react-router-dom";
 import BooksList from "./pages/BooksList";
 import SearchBooks from "./pages/SearchBooks";
 import * as BooksAPI from "./utils/BooksAPI";
+import _ from "lodash";
 
 class BooksApp extends React.Component {
-    state = {
-        books: []
-    };
-    async componentDidMount() {
-        this.setState({
-            books: await BooksAPI.getAll()
-        });
-    }
-    handleChange = async (bookId, shelf) => {
-        BooksAPI.update({ id: bookId }, shelf);
-            this.setState({
-                books: this.state.books.filter(book => book.id !== bookId).concat( await BooksAPI.get(bookId))
-            });
-    };
+  state = {
+    books: []
+  };
+  async componentDidMount() {
+    this.setState({
+      books: await BooksAPI.getAll()
+    });
+  }
+  handleChange = (bookHandle, bookShelf) => {
+    BooksAPI.update({ id: bookHandle.id }, bookShelf);
+    this.setState({
+      books: this.state.books
+        .filter(book => book.id !== bookHandle.id)
+        .concat(_.set(bookHandle, "shelf", bookShelf))
+    });
+  };
   render() {
-      const {books}=this.state;
+    const { books } = this.state;
     return (
       <div className="app">
-          {console.log(books)}
-          <Route exact path="/" render={()=>(
-              <BooksList books={books} handleChange={this.handleChange} />
-          )} />
-          <Route path="/search" render={()=>(
-              <SearchBooks books={books} handleChange={this.handleChange}/>
-          )}/>
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <BooksList books={books} handleChange={this.handleChange} />
+          )}
+        />
+        <Route
+          path="/search"
+          render={() => (
+            <SearchBooks books={books} handleChange={this.handleChange} />
+          )}
+        />
       </div>
     );
   }
