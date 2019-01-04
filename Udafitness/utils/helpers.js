f9e6b54723b6477c4b27cca0cd8bd5f92803b33f
LeoMosiah <leonardomosiah@gmail.com>
2019-01-04 10:22:19 -0200

    teste

--- Udafitness/utils/helpers.js
+++ Udafitness/utils/helpers.js
@@ -1,18 +1,16 @@
-import React from 'react';
-import { View } from 'react-native';
-import {FontAwesome,MaterialIcons,MaterialCommunityIcons} from '@expo/vector-icons'
-import { white} from './colors'
+import React from 'react'
+import { View } from 'react-native'
+import { FontAwesome, MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons'
+import { white } from './colors'
 
 export function isBetween (num, x, y) {
-  if (num >= x && num <= y) {
-    return true
-  }
+  return num >= x && num <= y;
 
-  return false
+
 }
 
 export function calculateDirection (heading) {
-  let direction = ''
+  let direction = '';
 
   if (isBetween(heading, 0, 22.5)) {
     direction = 'North'
@@ -45,28 +43,101 @@ export function timeToString (time = Date.now()) {
   return todayUTC.toISOString().split('T')[0]
 }
 
-export function getMetricMetaInfo () {
-  const info =  {
+export function getMetricMetaInfo (metric) {
+  const info = {
     run: {
-      displayName : 'Run',
+      displayName: 'Run',
       max: 50,
-      unit:"miles",
+      unit: 'miles',
       step: 1,
       type: 'steppers',
-      getIcon() {}
-      return (
-          <View>
+      getIcon() {
+        return (
+            <View>
               <MaterialIcons
                   name='directions-run'
-                  color={'black'}
+                  color={white}
                   size={35}
               />
-          </View>
-      )
+            </View>
+        )
+      }
     },
-    bike:{},
-    swim:{},
-    sleep:{},
-    eat:{}
+    bike: {
+      displayName: 'Bike',
+      max: 100,
+      unit: 'miles',
+      step: 1,
+      type: 'steppers',
+      getIcon() {
+        return (
+            <View>
+              <MaterialCommunityIcons
+                  name='bike'
+                  color={white}
+                  size={32}
+              />
+            </View>
+        )
+      }
+    },
+    swim: {
+      displayName: 'Swim',
+      max: 9900,
+      unit: 'meters',
+      step: 100,
+      type: 'steppers',
+      getIcon() {
+        return (
+            <View>
+              <MaterialCommunityIcons
+                  name='swim'
+                  color={white}
+                  size={35}
+              />
+            </View>
+        )
+      }
+    },
+    sleep: {
+      displayName: 'Sleep',
+      max: 24,
+      unit: 'hours',
+      step: 1,
+      type: 'slider',
+      getIcon() {
+        return (
+            <View>
+              <FontAwesome
+                  name='bed'
+                  color={white}
+                  size={30}
+              />
+            </View>
+        )
+      }
+    },
+    eat: {
+      displayName: 'Eat',
+      max: 10,
+      unit: 'rating',
+      step: 1,
+      type: 'slider',
+      getIcon() {
+        return (
+            <View>
+              <MaterialCommunityIcons
+                  name='food'
+                  color={white}
+                  size={35}
+              />
+            </View>
+        )
+      }
+    },
   }
+
+  return typeof metric === 'undefined'
+      ? info
+      : info[metric]
 }
